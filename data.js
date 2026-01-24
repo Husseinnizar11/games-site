e82b910ba3767b86fffbe33d91113fe87c08c1fa
Husseinnizar11 <h07734662979@gmail.com>
2026-01-24 17:29:18 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,18 @@
-
+const games = [
+  {
+    name: "God of War",
+    image: "https://cdromance.org/wp-content/uploads/2018/05/58603_front.jpg",
+    description: "لعبة أكشن مشهورة من سوني",
+    size: "15GB",
+    format: "ISO",
+    download: "https://linkvertise.com/xxxx"
+  },
+  {
+    name: "مغامرة",
+    image: "https://cdromance.org/wp-content/uploads/2018/05/58603_front.jpg",
+    description: "مغامرة في الغابة مع أصدقائك",
+    size: "500MB",
+    format: "EXE",
+    download: "https://linkvertise.com/yyyy"
+  }
+];
