1b347e9874a47b6b57c4955357c0cec3b96af51a
Husseinnizar11 <h07734662979@gmail.com>
2026-01-25 23:57:04 +0300

    Update data.js

--- data.js
+++ data.js
@@ -36,4 +36,13 @@ const games = [
     download: "#",
     category: "Mario-Maker-Stage"
   },
+  {
+    name: "Bring You Home",
+    image: "https://liteapks.com/wp-content/uploads/2024/10/bring-you-home-150x150.webp",
+    description: "لعبة الغاز مليئة بالإثارة",
+    size: "140MB",
+    format: "apk",
+    download: "https://download.gamercdn.top/download/air.com.alikestudio.bringyouhome-v1.0.60-MOD-gamekillerapp.com.apk",
+    category: "apk-Games"
+  },
 ];
