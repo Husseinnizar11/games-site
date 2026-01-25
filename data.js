dbc238af2752324aa6f662a2cacf0dbe393c80b3
Husseinnizar11 <h07734662979@gmail.com>
2026-01-25 23:53:50 +0300

    Update data.js

--- data.js
+++ data.js
@@ -19,12 +19,12 @@ const games = [
     category: "apk-app"
   },
   {
-    name: "لعبة أكشن",
-    image: "https://cdromance.org/wp-content/uploads/2018/05/58603_front.jpg",
-    description: "لعبة أكشن مليئة بالإثارة",
-    size: "900MB",
-    format: "ISO",
-    download: "#",
+    name: "Bring You Home",
+    image: "https://liteapks.com/wp-content/uploads/2024/10/bring-you-home-150x150.webp",
+    description: "لعبة الغاز مليئة بالإثارة",
+    size: "140MB",
+    format: "apk",
+    download: "https://download.gamercdn.top/download/air.com.alikestudio.bringyouhome-v1.0.60-MOD-gamekillerapp.com.apk",
     category: "apk-Games"
   },
   {
