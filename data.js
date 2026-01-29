55743ac5b5e1637fb29ec50f1503e936e1a9e838
1996wajdy-source <1996wajdy@gmail.com>
2026-01-29 23:11:23 +0300

    Update data.js

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ const games = [
   },
   {
     name: "تفعيل الكل في1",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW_xYOuzdl1M5iHhYRYKO1m7PIYhphIaBs_BIGKgs_uQ&s",
+   imgSrc: "images/key.png",
     description: "MAS_AIO.cmd  يفعل اوفس و وندوز حسب الرقم المكتوب",
     size: "1MB",
     format: "RAR",
