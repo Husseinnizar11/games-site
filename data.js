369731b4f78f618ca18465432b3d8ef28efff66e
1996wajdy-source <1996wajdy@gmail.com>
2026-02-04 11:50:11 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,255 +1,255 @@
 // ملف البيانات
 const games = [
 {
-name:"Sonic Unleashed",
-imgSrc:"images/SonicUnleashed.jpg",
-description:"سونيك أنليشد",
-size:"2.33GB",
-format:"ul",
-download:"https://linkjust.com/2bas7JNvkWe47HayvdiKHXqzl",
-category:"ps2-Games"
+name: "Sonic Unleashed",
+imgSrc: "images/SonicUnleashed.jpg",
+description: "سونيك أنليشد",
+size: "2.33GB",
+format: "ul",
+download: "https://linkjust.com/2bas7JNvkWe47HayvdiKHXqzl",
+category: "ps2-Games"
 },
 {
-name:"Sengoku Basara 2 Heroes",
-imgSrc:"images/Sengoku Basara 2 Heroes.jpg",
-description:" سينغوكو باسارا 2 هيروز",
-size:"3.83GB",
-format:"ul",
-download:"https://linkjust.com/K4DdtJsmScrClc6q",
-category:"ps2-Games"
+name: "Sengoku Basara 2 Heroes",
+imgSrc: "images/Sengoku Basara 2 Heroes.jpg",
+description: "سينغوكو باسارا 2 هيروز",
+size: "3.83GB",
+format: "ul",
+download: "https://linkjust.com/K4DdtJsmScrClc6q",
+category: "ps2-Games"
 },
 {
-name:"Ratchet & Clank",
-imgSrc:"images/Ratchet&Clank.jpg",
-description:"راتشيت آند كلانك: غوينغ كوماندو",
-size:"3.42GB",
-format:"ul",
-download:"https://linkjust.com/gcSpOM4y8krEYGNv5S",
-category:"ps2-Games"
+name: "Ratchet & Clank",
+imgSrc: "images/Ratchet &Clank.jpg",
+description: "راتشيت آند كلانك: غوينغ كوماندو",
+size: "3.42GB",
+format: "ul",
+download: "https://linkjust.com/gcSpOM4y8krEYGNv5S",
+category: "ps2-Games"
 },
 {
-name:"Ghost Rider",
-imgSrc:"images/GhostRider.jpg",
+name: "Ghost Rider",
+imgSrc: "images/GhostRider.jpg",
 description: "الشبح الراكب",
-size:"2.3GB",
-format:"ul",
-download:"https://linkjust.com/nklrqeakE0Vd",
-category:"ps2-Games"
+size: "2.3GB",
+format: "ul",
+download: "https://linkjust.com/nklrqeakE0Vd",
+category: "ps2-Games"
 },
 {
-name:"Metal Gear Solid 2",
-imgSrc:"images/MetalGearSolid.jpg",
-description:"ميتال جير سوليد 2",
-size:"3.2GB",
-format:"ul",
-download:"https://linkjust.com/bHiRbuH8OGJoGukzfqoZE",
-category:"ps2-Games"
+name: "Metal Gear Solid 2",
+imgSrc: "images/MetalGearSolid.jpg",
+description: "ميتال جير سوليد 2",
+size: "3.2GB",
+format: "ul",
+download: "https://linkjust.com/bHiRbuH8OGJoGukzfqoZE",
+category: "ps2-Games"
 },
 {
-name:"Metal Gear Solid 3",
-imgSrc:"images/Metal-Gear-Solid-3-Snake-Eater-1.jpg",
-description:"ميتال جير سوليد 3",
-size:"3.36GB",
-format:"ul",
-download:"https://linkjust.com/bYNAN3xXipMXUnMTpmdtoLef",
-category:"ps2-Games"
+name: "Metal Gear Solid 3",
+imgSrc: "images/Metal-Gear-Solid-3-Snake-Eater-1.jpg",
+description: "ميتال جير سوليد 3",
+size: "3.36GB",
+format: "ul",
+download: "https://linkjust.com/bYNAN3xXipMXUnMTpmdtoLef",
+category: "ps2-Games"
 },
 {
-name:"Ben 10",
-imgSrc:"images/Ben-10.jpg",
-description:"بن 10",
-size:"1.77GB",
-format:"ul",
-download:"https://linkjust.com/sU5",
-category:"ps2-Games"
+name: "Ben 10",
+imgSrc: "images/Ben-10.jpg",
+description: "بن 10",
+size: "1.77GB",
+format: "ul",
+download: "https://linkjust.com/sU5",
+category: "ps2-Games"
 },
 {
-name:"shadow of the colossus",
-imgSrc:"images/Shadow.jpeg",
-description:"ظل العملاق ",
-size:"2.3GB ",
-format:"ul ",
-download:"https://linkjust.com/jvk9F",
-category:"ps2-Games"
+name: "shadow of the colossus",
+imgSrc: "images/Shadow.jpeg",
+description: "ظل العملاق",
+size: "2.3GB",
+format: "ul",
+download: "https://linkjust.com/jvk9F",
+category: "ps2-Games"
 },
 {
-name:"Subway Surfers ",
-imgSrc:"images/su.jpg ",
-description:"لعبة القطار ",
-size:"77mb ",
-format:"apk",
-download:"https://up.apkbaba.com/games/Subway-Surfers-3.8.0-Mod(Apkbaba.com).apk",
-category:"apk-Games"
+name: "Subway Surfers",
+imgSrc: "images/su.jpg",
+description: "لعبة القطار",
+size: "77mb",
+format: "apk",
+download: "https://up.apkbaba.com/games/Subway-Surfers-3.8.0-Mod(Apkbaba.com).apk",
+category: "apk-Games"
 },
 {
-name:"gta san andreas ",
-imgSrc:"images/gta-sa.webp ",
-description:"غنية عن التعريف سي جي gta ",
-size:"2.2GB ",
-format:"apk ",
-download:"https://file.gtsamod.com/grand-theft-auto-san-andreas-v2.11.311-mod.apk",
-category:"apk-Games"
+name: "gta san andreas",
+imgSrc: "images/gta-sa.webp",
+description: "غنية عن التعريف سي جي gta",
+size: "2.2GB",
+format: "apk",
+download: "https://file.gtsamod.com/grand-theft-auto-san-andreas-v2.11.311-mod.apk",
+category: "apk-Games"
 },
 {
-name:"تفعيل الكل في1 ",
-imgSrc:"images/key.png ",
-description:"MAS_AIO.cmd  يفعل اوفس و وندوز حسب الرقم المكتوب ",
-size:"1MB ",
-format:"RAR ",
-download:"https://dev.azure.com/massgrave/Microsoft-Activation-Scripts/_apis/git/repositories/Microsoft-Activation-Scripts/items?$format=zip",
-category:"exe"
+name: "تفعيل الكل في1",
+imgSrc: "images/key.png",
+description: "MAS_AIO.cmd يفعل اوفس و وندوز حسب الرقم المكتوب",
+size: "1MB",
+format: "RAR",
+download: "https://dev.azure.com/massgrave/Microsoft-Activation-Scripts/_apis/git/repositories/Microsoft-Activation-Scripts/items?$format=zip",
+category: "exe"
 },
 {
-name:"office 2016",
-imgSrc:"images/office.jpg",
-description:" اوفس عربي 2016 كامل",
-size:"2GB",
-format:"RAR",
-download:"https://www.mediafire.com/file/y3mhdym14os925u/Microsoft_Office_2016_Pro_Plus%28AR%29.rar/file",
-category:"exe"
+name: "office 2016",
+imgSrc: "images/office.jpg",
+description: "اوفس عربي 2016 كامل",
+size: "2GB",
+format: "RAR",
+download: "https://www.mediafire.com/file/y3mhdym14os925u/Microsoft_Office_2016_Pro_Plus%28AR%29.rar/file",
+category: "exe"
 },
 {
-name:"Bring You Home ",
-imgSrc:"images/Bring.jpg ",
-description:"لعبة الغاز مليئة بالإثارة ",
-size:"140MB ",
-format:"apk ",
-download:"https://cdn.topmongo.com/packages/air.com.alikestudio.bringyouhome/Bring_You_Home_1.0.60_1000060_ad1727.apk",
-category:"apk-Games"
+name: "Bring You Home",
+imgSrc: "images/Bring.jpg",
+description: "لعبة الغاز مليئة بالإثارة",
+size: "140MB",
+format: "apk",
+download: "https://cdn.topmongo.com/packages/air.com.alikestudio.bringyouhome/Bring_You_Home_1.0.60_1000060_ad1727.apk",
+category: "apk-Games"
 },
 {
-name:"Lost in Play  ",
-imgSrc:"images/lost.jpg ",
-description:"مراحل و الغاز جميلة ",
-size:"700MB ",
-format:"apk ",
-download:"https://cdn.topmongo.com/packages/com.snapbreak.lip/Lost_in_Play_1.0.2061_2061_4348d3.apk",
-category:"apk-Games"
+name: "Lost in Play",
+imgSrc: "images/lost.jpg",
+description: "مراحل و الغاز جميلة",
+size: "700MB",
+format: "apk",
+download: "https://cdn.topmongo.com/packages/com.snapbreak.lip/Lost_in_Play_1.0.2061_2061_4348d3.apk",
+category: "apk-Games"
 },
 {
-name:"Modern Combat 5 ",
-imgSrc:"images/mc.webp ",
-description:"حرب و اكشن ",
-size:"90MB ",
-format:"apk ",
-download:"https://cdn.topmongo.com/packages/com.gameloft.android.ANMP.GloftM5HM/Modern_Combat_5_6.0.1a_60123_7bb3ae.apks",
-category:"apk-Games"
+name: "Modern Combat 5",
+imgSrc: "images/mc.webp",
+description: "حرب و اكشن",
+size: "90MB",
+format: "apk",
+download: "https://cdn.topmongo.com/packages/com.gameloft.android.ANMP.GloftM5HM/Modern_Combat_5_6.0.1a_60123_7bb3ae.apks",
+category: "apk-Games"
 },
 {
-name:"mario maker ",
-imgSrc:"images/smm.png ",
-description:" ماريو ميكر للجوال ",
-size:"236MB ",
-format:"apk ",
-download:"https://www.mediafire.com/file/n3y1pcxwwhe0mgt/SMM_WE_3.5.1_Keep_Aspect_Ratio.apk/file",
-category:"apk-Games"
+name: "mario maker",
+imgSrc: "images/smm.png",
+description: "ماريو ميكر للجوال",
+size: "236MB",
+format: "apk",
+download: "https://www.mediafire.com/file/n3y1pcxwwhe0mgt/SMM_WE_3.5.1_Keep_Aspect_Ratio.apk/file",
+category: "apk-Games"
 },
 {
-name:"minecraft",
-imgSrc:"images/mi.png",
-description:" ماين كرافت للجوال",
-size:"588MB",
-format:"apk",
-download:"https://www.mediafire.com/file/k6s3u04jo6xc8bu/minecraft-1.21.132.1-by-Baybars.apk/file",
-category:"apk-Games"
+name: "minecraft",
+imgSrc: "images/mi.png",
+description: "ماين كرافت للجوال",
+size: "588MB",
+format: "apk",
+download: "https://www.mediafire.com/file/k6s3u04jo6xc8bu/minecraft-1.21.132.1-by-Baybars.apk/file",
+category: "apk-Games"
 },
 {
-name:"among us",
-imgSrc:"images/am.png",
-description:"امونغ اس",
-size:"687MB",
-format:"apk",
-download:"https://cdn.chadshost.com/downloads/Among_Us_2025.12.11_030d6f6e_techylist.com.apk",
-category:"apk-Games"
+name: "among us",
+imgSrc: "images/am.png",
+description: "امونغ اس",
+size: "687MB",
+format: "apk",
+download: "https://cdn.chadshost.com/downloads/Among_Us_2025.12.11_030d6f6e_techylist.com.apk",
+category: "apk-Games"
 },
 {
-name:" NES+e+SNES  ",
-imgSrc:"images/NESeSNES.jpg",
-description:"NES+e+SNES",
-size: "",
-format:" ul ",
-download:"https://linkjust.com/0GTU2r5hxya5",
-category:"ps2-Games"
+name: "NES+e+SNES",
+imgSrc: "images/NESeSNES.jpg",
+description: "NES+e+SNES",
+size: "1.8GB",
+format: "ul",
+download: "https://linkjust.com/0GTU2r5hxya5",
+category: "ps2-Games"
 },
 {
-name:" Medal Of Honor Vanguard  ",
-imgSrc:"images/MedalOfHonorVanguard.jpg",
-description:"Medal Of Honor Vanguard",
-size:"",
-format:" ul ",
-download:"https://linkjust.com/av2slvZxNAr5wXdtpq",
-category:"ps2-Games"
+name: "Medal Of Honor Vanguard",
+imgSrc: "images/MedalOfHonorVanguard.jpg",
+description: "Medal Of Honor Vanguard",
+size: "4.2GB",
+format: "ul",
+download: "https://linkjust.com/av2slvZxNAr5wXdtpq",
+category: "ps2-Games"
 },
 {
-name:" wwe 2022  ",
-imgSrc:"images/Wwe2022.jpg",
-description:"wwe 2022",
-size: "",
-format:"ul",
-download:"https://linkjust.com/deJuX6WZt4UpZB7zFG",
-category:"ps2-Games"
+name: "wwe 2022",
+imgSrc: "images/Wwe2022.jpg",
+description: "wwe 2022",
+size: "2.8GB",
+format: "ul",
+download: "https://linkjust.com/deJuX6WZt4UpZB7zFG",
+category: "ps2-Games"
 },
 {
-name:" Prince of Persia: The Two Thrones  ",
-imgSrc:"images/PrinceOfPersiaTheTwoThrones.jpg",
-description:"Prince of Persia: The Two Thrones",
-size:"",
-format:"ul",
-download:"https://linkjust.com/z4g8I8Bb",
-category:"ps2-Games"
+name: "Prince of Persia: The Two Thrones",
+imgSrc: "images/PrinceOfPersiaTheTwoThrones.jpg",
+description: "Prince of Persia: The Two Thrones",
+size: "2.5GB",
+format: "ul",
+download: "https://linkjust.com/z4g8I8Bb",
+category: "ps2-Games"
 },
 {
-name:" Hitman 2",
-imgSrc:"images/Hitman2.jpg",
-description:"Hitman 2",
-size: "",
-format: " ul ",
-download:"https://linkjust.com/oavpCCDHT0FhC2dO26",
-category:"ps2-Games"
+name: "Hitman 2",
+imgSrc: "images/Hitman2.jpg",
+description: "Hitman 2",
+size: "3.0GB",
+format: "ul",
+download: "https://linkjust.com/oavpCCDHT0FhC2dO26",
+category: "ps2-Games"
 },
 {
-name:" bully gta  ",
-imgSrc:"images/BullyGta.jpg",
-description:"bully gta",
-size:"",
-format:"ul",
-download:"https://linkjust.com/cIsMbhdbeHBlUtVFFNCPCwbqj",
-category:"ps2-Games"
+name: "bully gta",
+imgSrc: "images/BullyGta.jpg",
+description: "bully gta",
+size: "2.1GB",
+format: "ul",
+download: "https://linkjust.com/cIsMbhdbeHBlUtVFFNCPCwbqj",
+category: "ps2-Games"
 },
 {
-name:" Conflict: Desert Storm 1  ",
-imgSrc:"images/ConflictDesertStorm1.jpg",
-description:"Conflict: Desert Storm 1",
-size: "",
-format: "ul ",
-download:"https://linkjust.com/vhMF95qtnlVs",
-category:"ps2-Games"
+name: "Conflict: Desert Storm 1",
+imgSrc: "images/ConflictDesertStorm1.jpg",
+description: "Conflict: Desert Storm 1",
+size: "2.2GB",
+format: "ul",
+download: "https://linkjust.com/vhMF95qtnlVs",
+category: "ps2-Games"
 },
 {
-name:" Conflict: Desert Storm2 ",
-imgSrc:"images/ConflictDesertStorm2.jpg",
-description:"Conflict: Desert Storm 2 ps2 ul",
-size: "",
-format: " ul ",
-download:"https://linkjust.com/5JJ",
-category:"ps2-Games"
+name: "Conflict: Desert Storm2",
+imgSrc: "images/ConflictDesertStorm2.jpg",
+description: "Conflict: Desert Storm 2 ps2 ul",
+size: "2.3GB",
+format: "ul",
+download: "https://linkjust.com/5JJ",
+category: "ps2-Games"
 },
 {
-name:" Prince of Persia  ",
-imgSrc:"images/PrinceOfPersia.jpg",
+name: "Prince of Persia",
+imgSrc: "images/PrinceOfPersia.jpg",
 description: "Prince of Persia ps2 ul",
-size: "",
-format:" ul ",
-download:"https://linkjust.com/dO6VgWPQaD6kN6XuWQvDCPyKG1v",
-category:"ps2-Games"
+size: "2.1GB",
+format: "ul",
+download: "https://linkjust.com/dO6VgWPQaD6kN6XuWQvDCPyKG1v",
+category: "ps2-Games"
 },
 {
-name:"tekken5 ",
-imgSrc:"images/Tekken5.jpg",
-description:"tekken5 ps2 ul",
+name: "tekken5",
+imgSrc: "images/Tekken5.jpg",
+description: "tekken5 ps2 ul",
 size: "3.73GB",
-format: " ul ",
-download:"https://linkjust.com/XgTsvc",
-category:"ps2-Games"
+format: "ul",
+download: "https://linkjust.com/XgTsvc",
+category: "ps2-Games"
 }
 ];
